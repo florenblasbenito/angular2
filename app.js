3d0ed149e84675c45c5ca8f4c927aebe6028ee86
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-04-12 12:48:56 +0200

    First Commit for Angular 2

---
+++ app.js
@@ -0,0 +1,9 @@
+// nombre tiene que ser una variable
+// de tipo text
+function saludar(nombre) {
+    console.log("Hola " + nombre.toUpperCase);
+}
+var wolverine = {
+    nombre: "Logan"
+};
+saludar(wolverine.nombre);
---
+++ app.ts
@@ -0,0 +1,14 @@
+// nombre tiene que ser una variable
+// de tipo text
+
+function saludar ( nombre:string ) {
+  console.log("Hola " + nombre.toUpperCase);
+
+
+}
+
+var wolverine = {
+  nombre: "Logan"
+
+};
+saludar (wolverine.nombre);
