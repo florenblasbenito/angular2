ecc3053f2cebc362c1ef2ae7ffaafc67b7e9cdb6
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-04-12 13:29:55 +0200

    new commit

--- typeScript6 ES6/app.js
+++ typeScript6 ES6/app.js
@@ -1,7 +1,7 @@
 // nombre tiene que ser una variable
 // de tipo text
 function saludar(nombre) {
-    console.log("Hola " + nombre.toUpperCase);
+    console.log("Hola " + nombre.toUpperCase());
 }
 var wolverine = {
     nombre: "Logan"
