19029b2e96516a9a3b73665ebd0f7399d09c13bd
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-05-05 11:07:43 +0200

    include cdn

---
+++ 1.HolaMundo/src/app/components/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `<h1>Header component {{name}}</h1>`
+})
+export class HeaderComponent { 
+
+
+
+
+ }
