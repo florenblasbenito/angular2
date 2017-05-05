38b08db8a9c36dfd3922d44c5501fb52d837a2c7
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-05-05 12:03:29 +0200

    changes_three

--- 1.HolaMundo/src/app/app.module.ts
+++ 1.HolaMundo/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
-import { HeaderComponent } from './components/header.component.js';
+import { HeaderComponent } from './components/header.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
