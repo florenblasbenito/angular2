ab07d35fefb007e0c19ec3aca39b9b8710f64527
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-05-10 12:36:27 +0200

    finish app

--- 2.SPA/src/app/components/buscador/buscador.component.ts
+++ 2.SPA/src/app/components/buscador/buscador.component.ts
@@ -10,6 +10,7 @@ import { HeroesService } from '../../services/heroes.service';
 export class BuscadorComponent implements OnInit {
 
   heroes:any[] = []
+  termino:string;
 
    constructor(private activatedRoute:ActivatedRoute,
    private _heroesService: HeroesService) {
@@ -21,7 +22,7 @@ export class BuscadorComponent implements OnInit {
   ngOnInit() {
 
     this.activatedRoute.params.subscribe( params => {
-      console.log(params['termino']);
+      this.termino =params['termino'];
       this.heroes = this._heroesService.buscarHeroes( params ['termino']);
       console.log(this.heroes)
     });
