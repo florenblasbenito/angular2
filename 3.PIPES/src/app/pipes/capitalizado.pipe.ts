2488044dce3e35a39e418f8ec4d776b2f8a08e2c
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-05-12 10:57:10 +0200

    capitalizar palabras_three

--- 3.PIPES/src/app/pipes/capitalizado.pipe.ts
+++ 3.PIPES/src/app/pipes/capitalizado.pipe.ts
@@ -2,12 +2,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'capitalizado'})
 export class CapitalizadoPipe implements PipeTransform {
-    transform(value: string, arg1, arg2, arg3 ): string {
+    transform(value: string, ...args: any[] ): string {
 
         console.log(value);
-        console.log(arg1);
-        console.log(arg2);
-        console.log(arg3);
+        console.log(args);
 
         return "Hola mundo!";
         
