bdb1a7e2776ef3eb5dd2d35b6e9b94be7af8bb59
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-05-12 10:55:32 +0200

    pipes personalizado_capitalizar palabras two

--- 3.PIPES/src/app/pipes/capitalizado.pipe.ts
+++ 3.PIPES/src/app/pipes/capitalizado.pipe.ts
@@ -2,10 +2,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'capitalizado'})
 export class CapitalizadoPipe implements PipeTransform {
-    transform(value: string, args: any[] ): string {
+    transform(value: string, arg1, arg2, arg3 ): string {
 
         console.log(value);
-        console.log(args);
+        console.log(arg1);
+        console.log(arg2);
+        console.log(arg3);
 
         return "Hola mundo!";
         
