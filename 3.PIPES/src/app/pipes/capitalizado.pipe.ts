9f8a8a3d412e21e88e121a91655dd22b38e624f5
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-05-12 11:51:29 +0200

    capitalizar palabras finish

--- 3.PIPES/src/app/pipes/capitalizado.pipe.ts
+++ 3.PIPES/src/app/pipes/capitalizado.pipe.ts
@@ -2,16 +2,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'capitalizado'})
 export class CapitalizadoPipe implements PipeTransform {
-    transform(value: string ): string {
+    transform(value: string, todas:boolean = true ): string {
 
         value = value.toLowerCase();
 
         let nombres = value.split("");
+
+        if (todas) {
         
         for ( let i in nombres) {
-            nombres[i] = nombres[i][0].toUpperCase() + nombres[i].substr(1)
-
+            nombres[i] = nombres[i][0].toUpperCase() + nombres[i].substr(1);
         }
+
+        } else {
+            nombres[0] = nombres[0][0].toUpperCase() + nombres[0].substr(1)
+        }
+
+        
         
 
 return nombres.join ("");
