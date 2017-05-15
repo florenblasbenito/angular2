26a343afded9645ac82e010dc3cf92dde327675c
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-05-15 11:33:47 +0200

    include components home and search

--- 4.SpotiApp/src/app/app.module.ts
+++ 4.SpotiApp/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
