393031c2e02843410866c6247803a63d483ef8d3
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-05-18 16:53:36 +0200

    add and deleted files

--- 5. Deseos - ionic/src/pages/tabs/tabs.ts
+++ 5. Deseos - ionic/src/pages/tabs/tabs.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 import { PendientesComponent } from '../pendientes/pendientes.component';
+import { TerminadosComponent } from '../terminados/terminados.component';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = PendientesComponent;
-  tab2Root = AboutPage;
+  tab1Root: any = PendientesComponent;
+  tab2Root: any = TerminadosComponent;
 
   constructor() {
 
