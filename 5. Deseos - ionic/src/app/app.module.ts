b342439754a868765c76aa1b422f3e6908fa1cbe
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-05-18 16:17:02 +0200

    add pendientes.component

--- 5. Deseos - ionic/src/app/app.module.ts
+++ 5. Deseos - ionic/src/app/app.module.ts
@@ -11,12 +11,15 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { PendientesComponent }  from '../pages/pendientes/pendientes.component';
+
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
     HomePage,
+    PendientesComponent,
     TabsPage
   ],
   imports: [
@@ -29,6 +32,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    PendientesComponent,
     TabsPage
   ],
   providers: [
---
+++ 5. Deseos - ionic/src/pages/pendientes/pendientes.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-pendientes',
+    templateUrl: 'pendientes.component.html',
+})
+export class PendientesComponent implements OnInit {
+    contructor() { }
+
+    ngOnInit() {}
+
+}
