3c7a7487df45cd00bc74907c634d10f9e6582731
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-05-18 10:04:20 +0200

    artista.components_two

--- 4.SpotiApp/src/app/components/artista/artista.component.ts
+++ 4.SpotiApp/src/app/components/artista/artista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-artista',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtistaComponent implements OnInit {
 
-  constructor() { }
+  constructor( private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+
+    this.activatedRoute.params
+    .map(parametros => parametros['id'])
+    .subscribe( id=> {
+        console.log(id);
+    })
   }
 
 }
