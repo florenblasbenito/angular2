0d0a38677be656da029fd0b9c548d601d2fa9e5d
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-05-22 15:19:00 +0200

    ngStyle_part four

--- 6. Miscelaneos/src/app/components/ng-style/ng-style.component.ts
+++ 6. Miscelaneos/src/app/components/ng-style/ng-style.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-ng-style',
   template: `
    
-   <p [ngStyle]="{'font-size': tamano + 'px'}">
+   <p [ngStyle]="{'font-size': tamano + 'px', 'color':'red' }">
    Hola mundo... esta es una etiqueta
    </p>
   `,
