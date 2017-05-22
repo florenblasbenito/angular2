b16c76632b8a5a39328fe602982796c14a35afa3
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-05-22 15:02:01 +0200

    add fontawesome

---
+++ 6. Miscelaneos/src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  template: `
+    <p>
+      ng-style Works!
+    </p>
+  `,
+  styles: []
+})
+export class NgStyleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
