f8a4ebd1e1c3002208c7efb9649d5c3f4aabda65
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-05-22 11:31:30 +0200

    implementando el  local storage

--- 5. Deseos - ionic/src/app/services/lista-deseos.service.ts
+++ 5. Deseos - ionic/src/app/services/lista-deseos.service.ts
@@ -9,16 +9,35 @@ export class ListaDeseosService {
 
     constructor() {
 
-        let lista1 = new Lista('Compras de supermercado');
-        let lista2 = new Lista('Juegos que deseo');
-        let lista3 = new Lista('Cosas de la universidad');
+     //   let lista1 = new Lista('Compras de supermercado');
+      //  let lista2 = new Lista('Juegos que deseo');
+       // let lista3 = new Lista('Cosas de la universidad');
 
 
-        this.listas.push( lista1 );
-        this.listas.push( lista2 );
-        this.listas.push( lista3 );
+       // this.listas.push( lista1 );
+       // this.listas.push( lista2 );
+       // this.listas.push( lista3 );
+
+       this.cargarData();
 
         console.log("Servicio inicializado!!");
+
+     }
+
+     actualizarData() {
+        localStorage.setItem ( "data", JSON.stringify(this.listas) );
+
+     }
+
+     cargarData() {
+         if( localStorage.getItem( "data") ) {
+        this.listas = JSON.parse(localStorage.getItem("data"));
+         }
+     }
+
+     agregarLista( lista:Lista ) {
+        this.listas.push(lista);
+        this.actualizarData();
 
      }
 }
