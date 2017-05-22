48b208cf59aff5e81b050304355e6151c885fb6f
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-05-22 13:02:20 +0200

    pantalla de detalle_ paso 1

--- 5. Deseos - ionic/src/pages/detalle/detalle.component.ts
+++ 5. Deseos - ionic/src/pages/detalle/detalle.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Lista, ListaItem } from '../../app/clases/index';
+import { ListaDeseosService } from '../../app/services/lista-deseos.service';
  
 @Component({
     selector: 'app-detalle',
@@ -10,12 +12,13 @@ import { NavController, NavParams } from 'ionic-angular';
 export class DetalleComponent implements OnInit {
 
     idx:number;
-    lista:any;
+    lista:Lista;
 
     constructor(
 
         public navCtrl:NavController,
-        public navParams:NavParams
+        public navParams:NavParams,
+        public _listaDeseos:ListaDeseosService
 
     ) { 
 
@@ -26,5 +29,14 @@ export class DetalleComponent implements OnInit {
     }
 
     ngOnInit() {}
+
+
+    actualizar( item:ListaItem ) {
+
+        item.completado = !item.completado;
+
+        this._listaDeseos.actualizarData();
+
+    }
 
 }
