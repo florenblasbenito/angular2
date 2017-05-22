c9d187418c1a65600868399cc71b84fb218d6e55
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-05-22 11:07:09 +0200

    agregando al servicio, alertas y retornando a la pantalla principal ok

--- 5. Deseos - ionic/src/pages/agregar/agregar.component.ts
+++ 5. Deseos - ionic/src/pages/agregar/agregar.component.ts
@@ -1,38 +1,70 @@
-import {Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Lista, ListaItem } from '../../app/clases/index';
+import { AlertController, NavController } from 'ionic-angular';
+
+import { ListaDeseosService } from '../../app/services/lista-deseos.service';
 
 
-@Component ({
+@Component({
     selector: 'app-agregar',
     templateUrl: 'agregar.component.html',
 })
 
 export class AgregarComponent implements OnInit {
 
-    nombreLista:string;
-    nombreItem:string = "";
+    nombreLista: string = "";
+    nombreItem: string = "";
 
-    items:ListaItem[] = [];
+    items: ListaItem[] = [];
 
-    constructor() { }
+    constructor(
+        public alertCtrl: AlertController,
+        public navCtrl: NavController,
+        public _listaDeseos: ListaDeseosService
 
-    ngOnInit() {}
+    ) { }
+
+    ngOnInit() { }
 
     agregar() {
-        if( this.nombreItem.length == 0 ) {
+        if (this.nombreItem.length == 0) {
             return;
 
         }
         let item = new ListaItem();
         item.nombre = this.nombreItem;
 
-        this.items.push( item );
+        this.items.push(item);
         this.nombreItem = "";
     }
 
-borrarItem( idx:number ){
-    this.items.splice(idx,1);
-}
+    borrarItem(idx: number) {
+        this.items.splice(idx, 1);
+    }
 
+    guardarLista() {
+        if (this.nombreLista.length == 0) {
+
+
+            let alert = this.alertCtrl.create({
+                title: 'Nombre de la lista',
+                subTitle: 'El nombre de la lista es necesario!',
+                buttons: ['OK']
+            });
+            alert.present();
+
+
+
+            return;
+        }
+
+
+        let lista = new Lista(this.nombreLista);
+        lista.items = this.items;
+
+        this._listaDeseos.listas.push(lista);
+        this.navCtrl.pop();
+
+    }
 
 }
